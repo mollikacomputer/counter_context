d69be575837ca34f2a031eb06475ef9ebc89051b
mollikacomputer <mollikacomputer3@gmail.com>
2023-02-03 11:11:03 +0600

    first commit

---
+++ src/pages/Parent.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import {COUNTER_CONTEXT} from "../App"
+const Parent = () => {
+const {count, setCount} = useContext(COUNTER_CONTEXT);
+  return (
+    <div className="container mx-auto ">
+      <div className="grid justify-center">
+        <h2 className="text-2xl my-16">counter {count} </h2>
+        <div className="flex justify-center gap-2">
+          <button
+            className="btn btn-primary"
+            onClick={()=> setCount(prvState => prvState+1)}
+          >
+            increment +
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={()=> setCount(prvState => prvState-1)}
+          >
+            Decrement -
+          </button>
+        </div>
+        <h2> This is home page</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Parent;
